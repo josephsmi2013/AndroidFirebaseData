4d7c68305e2c5c8028db28b72d416d9240002fe4
Joseph Smith <josephsmi2013@gmail.com>
2021-01-10 13:25:16 -0700

    Add project files.

---
+++ AndroidFirebaseData/Data Models/Alumini.cs
@@ -0,0 +1,11 @@
+﻿namespace AndroidFirebaseData.Data_Models
+{
+    public class Alumini
+    {
+        public string FullName { get; set; }
+        public string Department { get; set; }
+        public string Status { get; set; }
+        public string Set { get; set; }
+        public string ID { get; set; }
+    }
+}
---
+++ AndroidFirebaseData/Helpers/AppDataHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase;
+using Firebase.Database;
+
+namespace AndroidFirebaseData.Helpers
+{
+    public static class AppDataHelper
+    {
+        public static FirebaseDatabase GetDatabase()
+        {
+
+            var app = FirebaseApp.InitializeApp(Application.Context);
+            FirebaseDatabase database;
+
+            if (app == null)
+            {
+                var option = new FirebaseOptions.Builder()
+                    .SetApplicationId("androidfirebasedata-9c8cc")
+                    .SetApiKey("AIzaSyAwOuTFxnhIgSpLs7As8lezNxw_5bbeWtA")
+                    .SetDatabaseUrl("https://androidfirebasedata-9c8cc.firebaseio.com")
+                    .SetStorageBucket("androidfirebasedata-9c8cc.appspot.com")
+                    .Build();
+
+                app = FirebaseApp.InitializeApp(Application.Context, option);
+                database = FirebaseDatabase.GetInstance(app);
+            }
+            else
+            {
+                database = FirebaseDatabase.GetInstance(app);
+            }
+
+            return database;
+        }
+    }
+}
